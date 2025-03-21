f384d70e69cf18974be2454f6b195c30bf419961
Diego Arrieta <diego.arrietacalle@outlook.com>
2025-03-21 09:41:20 -0500

    create app

---
+++ RevisionModelos/RevisionModelos/Application.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RevitDB = Autodesk.Revit.DB;
+using RevitUI = Autodesk.Revit.UI;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using RevisionModelos.Forms;
+
+namespace RevisionModelos
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Application : RevitUI.IExternalCommand
+    {
+        public RevitUI.Result Execute(RevitUI.ExternalCommandData commandData, ref string message, RevitDB.ElementSet elements)
+        {
+            RevitUI.UIDocument uiDocument = commandData.Application.ActiveUIDocument;
+            RevitDB.Document document = uiDocument.Document;
+
+            FormApp app = new FormApp(uiDocument, document);
+            app.ShowDialog();
+
+            return RevitUI.Result.Succeeded;
+
+            if (false)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ RevisionModelos/RevisionModelos/Forms/FormApp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RevitDB = Autodesk.Revit.DB;
+using RevitUI = Autodesk.Revit.UI;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+
+namespace RevisionModelos.Forms
+{
+    public partial class FormApp : Form
+    {
+        RevitUI.UIDocument uiDocument;
+        RevitDB.Document document;
+        public FormApp(RevitUI.UIDocument uiDocument, RevitDB.Document document)
+        {
+            this.uiDocument = uiDocument;
+            this.document = document;
+
+            InitializeComponent();
+        }
+    }
+}
